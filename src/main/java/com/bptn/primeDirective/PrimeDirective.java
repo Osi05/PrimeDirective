1b897f5612def4e59923bce754de59a298032f55
Callistus <c.o.enekhaze@gmail.com>
2025-03-17 21:36:42 -0400

    worked on methods to filter both odd and even numbers

--- src/main/java/com/bptn/primeDirective/PrimeDirective.java
+++ src/main/java/com/bptn/primeDirective/PrimeDirective.java
@@ -3,60 +3,91 @@ package com.bptn.primeDirective;
 import java.util.ArrayList;
 
 public class PrimeDirective {
-	
-	//creating method for isPrime
-	
+
+	// creating method for isPrime
+
 	public boolean isPrime(int number) {
-		
-		//checking for numbers that's equals or less than 2
-		
+
+		// checking for numbers that's equals or less than 2
+
 		if (number == 2) {
 			return true;
-		}
-		else if (number < 2) {
+		} else if (number < 2) {
 			return false;
 		}
-		
-		
+
 		for (int i = 2; i < number; i++) {
-			
+
 			if (number % i == 0) {
-				
+
 				return false;
 			}
 		}
 		return true;
 	}
-	
-	//creating onlyPrime method
-	
-	public ArrayList<Integer> onlyPrimes(int[] numbers){
-		
-		//creating empty ArrayList
+
+	// creating onlyPrime method
+
+	public ArrayList<Integer> onlyPrimes(int[] numbers) {
+
+		// creating empty ArrayList
 		ArrayList<Integer> primes = new ArrayList<Integer>();
-		
-		//using forEach to access the ArrayList
+
+		// using forEach to access the ArrayList
 		for (int number : numbers) {
-			
-			
-			if(isPrime(number)) {
+
+			if (isPrime(number)) {
 				primes.add(number);
 			}
 		}
-		
+
 		return primes;
-		
+
 	}
-	
-	
+
+	// method to filter an array for even numbers
+
+	public ArrayList<Integer> filterEven(int[] numbers) {
+
+		// ArrayList to save the even numbers
+		ArrayList<Integer> evenNumbers = new ArrayList<Integer>();
+
+		for (int number : numbers) {
+
+			if (number % 2 == 0) {
+				evenNumbers.add(number);
+
+			}
+		}
+		return evenNumbers;
+	}
+
+	// method to filter an array for odd numbers
+	public ArrayList<Integer> filterOdd(int[] numbers) {
+
+		// ArrayList to save the odd numbers
+		ArrayList<Integer> oddNumbers = new ArrayList<Integer>();
+
+
+		for (int number : numbers) {
+
+			if (number % 2 != 0) {
+
+				oddNumbers.add(number);
+			}
+		}
+
+		return oddNumbers;
+	}
+
 	public static void main(String[] args) {
-		
+
 		PrimeDirective numTest = new PrimeDirective();
-		
-		int[] nums = {6, 29, 28, 33, 11, 100, 101, 43, 89};
-		
-		System.out.println(numTest.onlyPrimes(nums)); 
-		
+
+		int[] nums = { 6, 29, 28, 33, 11, 100, 101, 43, 89 };
+
+		System.out.println(numTest.filterOdd(nums));
+
 	}
 
 }
